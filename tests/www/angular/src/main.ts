061bff483c728dc559837fdeb5131317f9ef8f40
Tiberiu Krisboi <tiberiu.krisboi@betfair.com>
2017-07-19 19:01:45 +0300

    fix unit tests ans some context issues after pull request

--- tests/www/angular/src/main.ts
+++ tests/www/angular/src/main.ts
@@ -2,16 +2,16 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app';
 
-// var isTesting = /^NG_DEFER_BOOTSTRAP!/.test(window.name);
-// var bootApp = () => {
-//     platformBrowserDynamic().bootstrapModule(AppModule);
-// };
-// if (!isTesting) {
-//     bootApp();
-// } else {
-//     // setting application boot function on window for protractor
-//     (<any>window).bootApp = bootApp;
-// }
-
+var isTesting = /^NG_DEFER_BOOTSTRAP!/.test(window.name);
+var bootApp = () => {
+    platformBrowserDynamic().bootstrapModule(AppModule);
+};
+if (!isTesting) {
+    bootApp();
+} else {
+    // setting application boot function on window for protractor
+    (<any>window).bootApp = bootApp;
+}
+console.log('Boot app');
 platformBrowserDynamic().bootstrapModule(AppModule);
 
