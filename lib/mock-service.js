krisboit/protractor-xmlhttprequest-mock

commit 5e61f3b9f9501bd9650e45b972d468c9502de540

Inject browser scripts when needed
Inject browser scripts in the `MockService.setup()` function, but only
if they are needed. This way, it is easy to install the `MockService`
and we don't have to rely on `onPageLoad()` from `lib/index.js` (which
isn't called in most environments).

It is now possible to use protractor-xmlhttprequest-mock like this:

    import {browser, $} from 'protractor';
    import {MockService} from 'protractor-xmlhttprequest-mock';

    describe('the backend', () => {
      it('should reply with code 418', () => {
        MockService.setup(browser);
        MockService.addMock('mock1', {
          path: '/api/teapot',
          response: {status: 418, data: "I'm a teapot"},
        });

        $('button').click();
        expect($('div').getText()).toEqual("I'm a teapot");
      });
    });

lib/mock-service.js
@@ -1,3 +1,6 @@
+var fs = require('fs'),
+    path = require('path');
+
 var MockService = {
     reset: function() {
         if (this.browser) {
@@ -9,16 +12,31 @@ var MockService = {
 
     setup: function(browser) {
         this.browser = browser;
-        if (this.queue) {
-            //console.log('Execute scripts from queue: ');
-            this.queue.forEach(function (script) {
-                //console.log(script);
-                this.browser.executeScript(script);
-            });
-            //console.log('DONE');
-            this.queue = [];
-        }
-        this.browser.executeScript("if (window.bootApp) window.bootApp();");
+
+        // Inject browser scripts if needed
+        return browser.executeScript('return typeof MockService == "undefined"')
+        .then(function (needed) {
+            if (needed) {
+                var scripts = "";
+                scripts += fs.readFileSync(path.join(
+                    __dirname, './browser-scripts/XMLHttpRequestMock.js'), 'utf-8');
+                scripts += fs.readFileSync(path.join(
+                    __dirname, './browser-scripts/MockManager.js'), 'utf-8');
+
+                browser.executeScript(scripts + 'MockManager.setup();');
+            }
+
+            if (this.queue) {
+                //console.log('Execute scripts from queue: ');
+                this.queue.forEach(function (script) {
+                    //console.log(script);
+                    this.browser.executeScript(script);
+                });
+                //console.log('DONE');
+                this.queue = [];
+            }
+            this.browser.executeScript("if (window.bootApp) window.bootApp();");
+        });
     },
 
     addMock: function(name, config) {

lib/index.js
@@ -1,18 +1,6 @@
-var fs = require('fs'),
-	path = require('path'),
-    MockService = require('./mock-service').MockService;
-
-// load scripts from files
-var scripts = "";
-scripts += fs.readFileSync(path.join(__dirname, './browser-scripts/XMLHttpRequestMock.js'), 'utf-8');
-scripts += fs.readFileSync(path.join(__dirname, './browser-scripts/MockManager.js'), 'utf-8');
+var MockService = require('./mock-service').MockService;
 
 exports.onPageLoad = function () {
-    // inject script in browser after page load
-    //console.log('setup');
-    browser.executeScript(
-        scripts + "MockManager.setup();"
-    );
     MockService.setup(browser);
 };
 

commit 257ad2278689b80a4dcd2ee1577ea09c1798c420

Fix browser scripts injection
This fixes a bug introduced by commit 5e61f3b: the mock object that
lives inside the browser is `MockManager`, not `MockService` (the latter
lives in the test process).

I'm sorry I wasn't careful enough when writing the original commit :/

lib/mock-service.js
@@ -16,7 +16,7 @@ var MockService = {
         var self = this;
 
         // Inject browser scripts if needed
-        return browser.executeScript('return typeof MockService == "undefined"')
+        return browser.executeScript('return typeof MockManager == "undefined"')
         .then(function (needed) {
             if (needed) {
                 //console.log('injecting mock service!');